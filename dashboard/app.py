4049cb1f80a47f3416ac345b442454d464f0e490
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-08-19 21:45:26 -0300

    fix(app.py): corrigi o caminho dos views no deploy

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,13 @@
 import streamlit as st
+from utils.config import obter_caminho_arquivo
 
-# Define as views
-main_page = st.Page("views/home.py", title="Página inicial")
-page_2 = st.Page("views/funcionarios.py", title="Despesas com os funcionários")
-#page_3 = st.Page("views/diarias.py", title="Despesas com Diárias")
-page_4 = st.Page("views/obras.py", title="Obras")
-page_5 = st.Page("views/patrimonio.py", title="Patrimônio")
-page6 = st.Page("views/orcamento.py", title="Orçado e Executado")
-
+# Define as views usando caminhos absolutos seguros
+main_page = st.Page(obter_caminho_arquivo('dashboard/views', 'home.py'), title="Página inicial")
+page_2 = st.Page(obter_caminho_arquivo('dashboard/views', 'funcionarios.py'), title="Despesas com os funcionários")
+#page_3 = st.Page(obter_caminho_arquivo('dashboard/views', 'diarias.py'), title="Despesas com Diárias")
+page_4 = st.Page(obter_caminho_arquivo('dashboard/views', 'obras.py'), title="Obras")
+page_5 = st.Page(obter_caminho_arquivo('dashboard/views', 'patrimonio.py'), title="Patrimônio")
+page6 = st.Page(obter_caminho_arquivo('dashboard/views', 'orcamento.py'), title="Orçado e Executado")
 
 pg = st.navigation([main_page, page_2, page_4, page_5, page6])
 
