e7c6d6f079c7b4a41d425b4ea9eec8f6eda68e6e
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-08-22 15:41:40 -0300

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from utils.config import obter_caminho_arquivo
 
+st.logo('https://public-blob.squarecloud.dev/c5fea93b0bd97005b83e0c46a5465f22c4b53eac/image/LogoOpenCitymetrics-ex180.jpeg'
+    'logo.png')
+
 # Define as views usando caminhos absolutos seguros
 main_page = st.Page(obter_caminho_arquivo('dashboard/views', 'home.py'), title="Página inicial")
 page_2 = st.Page(obter_caminho_arquivo('dashboard/views', 'funcionarios.py'), title="Despesas com os funcionários")
