612e2c1fa6ca46801eea4e9f69288e2d88d1a08b
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-09-07 11:37:25 -0300

    refactor(models): Altera o nome da classe DadosFuncionarios para  DadosFuncionario

--- database/models/funcionario.py
+++ database/models/funcionario.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from database.connection import Base
 
 
-class DadosFuncionarios(Base):
+class DadosFuncionario(Base):
     __tablename__ = "dados_funcionarios"
 
     __table_args__ = (
